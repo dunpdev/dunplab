380948a4ac9df6a550f58a4bcf9e297fb7f56f40
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-12-07 17:12:24 +0100

    Add TransferRezultati service class that implements Transfer function from the ITransferRezultati interface

---
+++ DUNPLab.API/Services/TransferRezultati.cs
@@ -0,0 +1,63 @@
+﻿using DUNPLab.API.Infrastructure;
+using DUNPLab.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Server;
+using System.Globalization;
+
+namespace DUNPLab.API.Services
+{
+    public class TransferRezultati : ITransferRezultati
+    {
+        private readonly DunpContext context;
+        private readonly ILogger<TransferRezultati> logger;
+
+        public TransferRezultati(DunpContext context,
+            ILogger<TransferRezultati> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public void Transfer()
+        {
+            logger.LogInformation("Beginning transfer. " + DateTime.UtcNow);
+            List<RezultatOdMasine> rezultatiOdMasine = context.RezultatiOdMasine
+                .Include(rm => rm.VrednostiOdMasine)
+                .Where(rm => rm.JesuLiPrebaceni == false)
+                .ToList();
+
+            foreach (RezultatOdMasine rm in rezultatiOdMasine)
+            {
+                foreach (VrednostOdMasine sr in rm.VrednostiOdMasine)
+                {
+                    Rezultat rezultat = new Rezultat();
+
+                    rezultat.JeLiUGranicama = sr.JeLiBiloGreske;
+                    rezultat.Vrednost = sr.Vrednost;
+                    rezultat.Uzorak = context.Uzorci.Where(u => u.KodEpruvete == rm.KodEpruvete).FirstOrDefault();
+                    if (rezultat.Uzorak == null)
+                    {
+                        throw new Exception("Ne postoji uzorak sa kodom epruvete: " +  rm.KodEpruvete);
+                    }
+                    rezultat.IdUzorka = rezultat.Uzorak.Id;
+                    rezultat.Uzorak.IzmenioDatumVreme = rm.DatumVreme;
+                    rezultat.Supstanca = context.Supstance
+                        .Where(s => s.Oznaka == sr.OznakaSubstance)
+                        .FirstOrDefault();
+                    if (rezultat.Supstanca == null)
+                    {
+                        throw new Exception("Ne postoji supstanca sa oznakom: " + sr.OznakaSubstance);
+                    }
+                    rezultat.IdSupstance = sr.Id;
+                    context.Rezultati.Add(rezultat);
+                }
+
+                rm.JesuLiPrebaceni = true;
+            }
+
+            context.SaveChanges();
+
+            logger.LogInformation("Transfer ended. " + DateTime.UtcNow);
+        }
+    }
+}
