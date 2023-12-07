05e2e18ee62c3437b92c15f3ba77cb95112aac90
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-12-07 18:55:01 +0100

    Introduce an error flag that turns to true if any Supstanca or Uzorak is non-existent. Then an exception will be thrown and the transfer for the entire RezultatOdMasine will be postponed until the missing data is introduced.

--- DUNPLab.API/Services/TransferRezultati.cs
+++ DUNPLab.API/Services/TransferRezultati.cs
@@ -26,32 +26,53 @@ namespace DUNPLab.API.Services
                 .Where(rm => rm.JesuLiPrebaceni == false)
                 .ToList();
 
+            bool error = false;
             foreach (RezultatOdMasine rm in rezultatiOdMasine)
             {
+                error = false;
                 foreach (VrednostOdMasine sr in rm.VrednostiOdMasine)
                 {
+                    try
+                    {
                     Rezultat rezultat = new Rezultat();
 
                     rezultat.JeLiUGranicama = sr.JeLiBiloGreske;
                     rezultat.Vrednost = sr.Vrednost;
-                    rezultat.Uzorak = context.Uzorci.Where(u => u.KodEpruvete == rm.KodEpruvete).FirstOrDefault();
+
+                        rezultat.Uzorak = context.Uzorci
+                            .Where(u => u.KodEpruvete == rm.KodEpruvete)
+                            .FirstOrDefault();
+
                     if (rezultat.Uzorak == null)
                     {
-                        throw new Exception("Ne postoji uzorak sa kodom epruvete: " +  rm.KodEpruvete);
+                            throw new Exception("Ne postoji uzorak sa datim kodom epruvete: " + rm.KodEpruvete);
                     }
-                    rezultat.IdUzorka = rezultat.Uzorak.Id;
-                    rezultat.Uzorak.IzmenioDatumVreme = rm.DatumVreme;
+
                     rezultat.Supstanca = context.Supstance
                         .Where(s => s.Oznaka == sr.OznakaSubstance)
                         .FirstOrDefault();
+
                     if (rezultat.Supstanca == null)
                     {
                         throw new Exception("Ne postoji supstanca sa oznakom: " + sr.OznakaSubstance);
                     }
+
+                        rezultat.IdUzorka = rezultat.Uzorak.Id;
+                        rezultat.Uzorak.IzmenioDatumVreme = rm.DatumVreme;
+
                     rezultat.IdSupstance = sr.Id;
                     context.Rezultati.Add(rezultat);
+                    catch (Exception ex)
+                    {
+                        logger.LogWarning(ex.Message);
+                        logger.LogWarning($"Zbog nedostatka podataka transfer rezulatata {rm.Id} je odlozen.");
+                        error = true;
+                        continue;
+                    }
                 }
 
+                if (!error)
+                {
                 rm.JesuLiPrebaceni = true;
             }
 
