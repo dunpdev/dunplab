239ee926c6e0594b92d3be630b38a518d8c856b9
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-12-07 17:36:21 +0100

    Revert "Add DbSets for RezultatOdMasine and VrednostOdMasine"
    This reverts commit 42ef8c1fad9da0f45e5983a66c95f0251556178d.

--- DUNPLab.API/Infrastructure/DunpContext.cs
+++ DUNPLab.API/Infrastructure/DunpContext.cs
@@ -13,7 +13,5 @@ namespace DUNPLab.API.Infrastructure
         public DbSet<Testiranje> Testiranja { get; set; }
         public DbSet<Supstanca> Supstance { get; set; }
         public DbSet<Rezultat> Rezultati { get; set; }
-        public DbSet<RezultatOdMasine> RezultatiOdMasine { get; set; }
-        public DbSet<VrednostOdMasine> VrednostiOdMasine { get; set; }
     }
 }
