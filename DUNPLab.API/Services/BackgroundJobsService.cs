1253bf9085e14191e90dd2211b3112c6885480a5
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-12-07 10:55:53 +0100

    finished task

--- DUNPLab.API/Services/BackgroundJobsService.cs
+++ DUNPLab.API/Services/BackgroundJobsService.cs
@@ -20,26 +20,16 @@ namespace DUNPLab.API.Services
 
         public async Task Rezultati()
         {
-            //Console.WriteLine("Rezultati");
-            //var unprocessedZahtevs = context.Zahtevi.Where(z => z.JeLiObradjen == false).Include(p => p.Pacijent).Include(z => z.ZahtevSupstance).ThenInclude(zs => zs.Supstanca).ToList();
             var unprocessedZahtevi = await context.Uzorci
                 .ProjectTo<UzorakFileDTO>(mapper.ConfigurationProvider)
                 .Where(u => u.JeLiObradjen == false)
                 .ToListAsync();
-            //var unprocessedZahtevi = context.Zahtevi
-            //    .Include(z => z.Testiranje)
-            //    .ThenInclude(t => t.Uzorci)
-            //    .Include(z => z.ZahtevSupstance)
-            //    .ThenInclude(zs => zs.Supstanca)
-            //    .Include(z => z.Pacijent)
-            //    .Where(z => z.JeLiObradjen == false)
-            //    .ToList();
 
             foreach (var uzorak in unprocessedZahtevi)
             {
                 var zahtev = context.Zahtevi.Find(uzorak.ZahtevId);
                 if (zahtev == null) continue;
-                var filePath = Path.Combine("wwwroot", "rezultati", $"{uzorak.ImePacijenta}{uzorak.PrezimePacijenta}{uzorak.KodEpruvete}.txt");
+                var filePath = Path.Combine("wwwroot", "rezultati", $"{uzorak.KodEpruvete}.txt");
                 using (var writer = new StreamWriter(filePath))
                 {
                     writer.WriteLine($"{uzorak.ImePacijenta}{uzorak.PrezimePacijenta},{uzorak.KodEpruvete},{DateTime.Now:yyyyMMddHHmmss}");
@@ -49,7 +39,6 @@ namespace DUNPLab.API.Services
                         var random = new Random();
                         var vrednost = random.NextDouble() * supstanca.GornjaGranica * 1.1;
                         var jeLiBiloGreske = vrednost > supstanca.GornjaGranica ? "true" : "false";
-                        //round the random num on 2 decimals
                         var pom = vrednost.ToString().Substring(0, 4);
                         writer.WriteLine($"{supstanca.Oznaka},{pom},{jeLiBiloGreske}");
                     }
