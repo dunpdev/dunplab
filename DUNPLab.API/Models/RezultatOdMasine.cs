7572b5ed2941ae010edd7cce354b36efbd1c095d
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-12-07 17:05:45 +0100

    Add RezultatOdMasine and VrednostOdMasine models

---
+++ DUNPLab.API/Models/RezultatOdMasine.cs
@@ -0,0 +1,13 @@
+﻿namespace DUNPLab.API.Models
+{
+    public class RezultatOdMasine
+    {
+        public int Id { get; set; }
+        public string ImeIPrezime { get; set; }
+        public string KodEpruvete { get; set; }
+        public DateTime DatumVreme { get; set; }
+        public bool JesuLiPrebaceni { get; set; } = false;
+        public ICollection<VrednostOdMasine> VrednostiOdMasine { get; set; }
+    }
+}
+
