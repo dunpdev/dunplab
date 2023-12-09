870c7368907ecca58c47741cfd27ea22fe84583b
vahid.uglic <vakson12@gmail.com>
2023-12-09 12:44:20 +0100

    Register jobs

--- DUNPLab.API/Program.cs
+++ DUNPLab.API/Program.cs
@@ -1,6 +1,9 @@
 using DUNPLab.API.Infrastructure;
+using DUNPLab.API.Jobs.PacijentiJobs;
+using DUNPLab.API.Services.Pacijenti;
 using Hangfire;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +17,17 @@ builder.Services.AddHangfire(config =>
     config.UseRecommendedSerializerSettings();
     config.UseSqlServerStorage(builder.Configuration.GetConnectionString("default"));
 });
-builder.Services.AddHangfireServer();
+builder.Services.AddScoped<IPacijentiService, PacijentiService>();
+builder.Services.AddScoped<IPacijentiJobRegistrator, PacijentiJobRegistrator>();
+builder.Services.AddHangfireServer((provider, serverOptions) =>
+{
+    using (var serviceScope = provider.CreateScope())
+    {
 
+        var pacijentiRegistrator = serviceScope.ServiceProvider.GetRequiredService<IPacijentiJobRegistrator>();
+        pacijentiRegistrator.Register();
+    }
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
