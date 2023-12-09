89bd3c4b064e253fcf67021960f0b0ef6d2b9a1a
vahid.uglic <vakson12@gmail.com>
2023-12-09 12:44:05 +0100

    Add PacijentiService

---
+++ DUNPLab.API/Services/Pacijenti/IPacijentiService.cs
@@ -0,0 +1,13 @@
+﻿using DUNPLab.API.Dtos;
+using DUNPLab.API.Models;
+
+namespace DUNPLab.API.Services.Pacijenti
+{
+    public interface IPacijentiService
+    {
+        Task Seed();
+        Task<List<PacijentDto>> ReadPatientsFromFileAsync();
+
+        Task<bool> CheckIfExistsBasedOnJmbg(string jmbg);
+    }
+}
