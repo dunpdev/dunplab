84e42c1d5cb921d4c8c05ad52a121d5e15126827
Ertan Muslic <ertanmuslic@gmail.com>
2023-12-10 22:38:32 +0100

    Register dependecy injection for IEmailService and schedule recuring job for sendEmail every 15 minutes

--- DUNPLab.API/Program.cs
+++ DUNPLab.API/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddHangfire(config =>
 });
 builder.Services.AddHangfireServer();
 builder.Services.AddTransient<ITransferRezultati, TransferRezultati>();
-
+builder.Services.AddTransient<IEmailService, EmailService>();
 builder.Services.AddTransient<IOdredjivanjeStatusa, OdredjivanjeStatusa>();
 builder.Services.AddTransient<IArhivirajPacijenteService, ArhivirajPacijenteService>();
 builder.Services.AddTransient<IEmailReportService, EmailReportService>();
@@ -57,6 +57,7 @@ RecurringJob.AddOrUpdate<IBackgroundJobsService>(x => x.Rezultati(), "0 */10 * *
 RecurringJob.AddOrUpdate<IOdredjivanjeStatusa>("odredjivanje-statusa", service => service.Odredi(), "*/5 * * * *");
 
 RecurringJob.AddOrUpdate<FileBackupService>(x => x.BackupFiles(), Cron.MinuteInterval(2));
+RecurringJob.AddOrUpdate<IEmailService>(x => x.SendEmail(), "*/15 * * * *");
 
 RecurringJob.AddOrUpdate<ITransferRezultati>("transfer-rezultata", service => service.Transfer(), "*/5 * * * *");
 RecurringJob.AddOrUpdate<IPacijentiService>("VahidovJob", service=>service.Seed(), "0 0 * * 0");
